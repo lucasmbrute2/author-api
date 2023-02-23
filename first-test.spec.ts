fce6ced456d3c9ce7b8de2a2e25e19cbb61e4895
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-23 20:47:03 -0300

    chore: delete initial test file

--- first-test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("random test", () => {
-    it("should be able to execute a simple sum test", () => {
-        expect(1 + 1).toEqual(2);
-    });
-});
