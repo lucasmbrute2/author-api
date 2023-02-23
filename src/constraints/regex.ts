4d5168ed2b844e5c389eab89e0ce54b42ee1bc16
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-23 20:47:32 -0300

    feat: add object to hold about regex logic

---
+++ src/constraints/regex.ts
@@ -0,0 +1,7 @@
+export const validation = {
+    passwordValidation: new RegExp(
+        /(?=^.{8,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g
+    ),
+    nameValidation: new RegExp(/^[a-zA-Z ]{3,20}$/),
+    emailValidation: new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+};
