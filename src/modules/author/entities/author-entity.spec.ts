89fa374fa5bd5da461db8bc0dde06b725ab51a77
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-23 20:49:56 -0300

    feat: Author name specs

--- src/modules/author/entities/author-entity.spec.ts
+++ src/modules/author/entities/author-entity.spec.ts
@@ -3,13 +3,24 @@ import { Author } from "./author-entity";
 import { Email, Name, Password } from "./validation";
 
 describe("Author entity", () => {
-    it("Should be able to instace an Author", () => {
+    it("should be able to instace an Author", () => {
         const author = new Author({
-            name: new Name("lucas"),
+            name: new Name("test name"),
             password: new Password("Test-password!2"),
             bio: "test bio",
             username: new Email("testemail@gnmail.com"),
         });
         expect(author).toBeTruthy();
+    });
+
+    it("should not be able to instace an Author with Name in a wrong format", () => {
+        expect(() => {
+            new Author({
+                name: new Name("test-name"),
+                password: new Password("Test-password!2"),
+                bio: "test bio",
+                username: new Email("testemail@gnmail.com"),
+            });
+        }).toThrow();
     });
 });
