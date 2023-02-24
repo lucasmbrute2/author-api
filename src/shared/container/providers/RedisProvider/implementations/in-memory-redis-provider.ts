cee9d994e33aa144f6b8f5121bd65c0afd8c9348
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-24 16:49:03 -0300

    feat: add In memory redis repository

---
+++ src/shared/container/providers/RedisProvider/implementations/in-memory-redis-provider.ts
@@ -0,0 +1,16 @@
+import { RedisRepository } from "@app/repositories/redis-repository";
+
+export class InMemoryRedisProvider implements RedisRepository {
+    public redisClient = new Map();
+
+    async connect(): Promise<void> {}
+    async disconnect(): Promise<void> {}
+
+    async setValue(key: string, value: string, expire: number): Promise<void> {
+        this.redisClient.set(key, value);
+    }
+
+    async getValue(key: string): Promise<string> {
+        return this.redisClient.get(key);
+    }
+}
