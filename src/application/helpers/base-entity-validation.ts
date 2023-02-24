08b6848cad0c782d394388d48c66ee641ff765b9
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-24 19:51:34 -0300

    fix: base input order of validations

--- src/application/helpers/base-entity-validation.ts
+++ src/application/helpers/base-entity-validation.ts
@@ -6,9 +6,8 @@ export class BaseEntityValidation {
         private readonly regex: RegExp,
         private readonly shouldValidate: boolean
     ) {
-        if (!input) throw new AppError("Input missing", 400);
-
         if (!this.shouldValidate) return;
+        if (!input) throw new AppError("Input missing", 400);
         if (!this.isInputValid())
             throw new AppError(
                 `Input ${this.input} has the wrong format.`,
