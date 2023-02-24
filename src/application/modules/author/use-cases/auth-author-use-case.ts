06c8f5131d144340b22489b0953689c370922863
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-24 19:51:58 -0300

    feat: add auth author use case

---
+++ src/application/modules/author/use-cases/auth-author-use-case.ts
@@ -0,0 +1,45 @@
+import "reflect-metadata";
+import { AuthorRepository } from "@app/repositories/author-repository";
+import { BadRequestError, NotFoundError } from "@shared/errors/app-error";
+import { inject, injectable } from "tsyringe";
+import { Email } from "../entities/validation";
+import { sign } from "jsonwebtoken";
+import { enviromentVariables } from "@app/constraints/enviroment-variables";
+
+interface AuthAuthorUseCaseProps {
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+interface AuthAuthorUseCaseResponse {
+    token: string;
+}
+
+@injectable()
+export class AuthAuthorUseCase {
+    constructor(
+        @inject("AuthorRepository")
+        private authorRepository: AuthorRepository
+    ) {}
+
+    async execute(
+        req: AuthAuthorUseCaseProps
+    ): Promise<AuthAuthorUseCaseResponse> {
+        if (req.password !== req.confirmPassword)
+            throw new BadRequestError("Password do not match");
+
+        const email = new Email(req.email);
+        const isAuthorExistent = await this.authorRepository.findByEmail(email);
+
+        if (!isAuthorExistent) throw new NotFoundError("Author not found");
+
+        const token = sign({}, enviromentVariables.jwtTokenHash, {
+            subject: JSON.stringify(isAuthorExistent.id),
+        });
+
+        return {
+            token,
+        };
+    }
+}
