b4131a1e1ddb6b6b7930c6bdea613324996023a0
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-24 00:29:44 -0300

    fix: remove bio

--- src/application/modules/author/factory/makeAuthor.ts
+++ src/application/modules/author/factory/makeAuthor.ts
@@ -5,7 +5,6 @@ type partialAuthor = Partial<Author>;
 
 export function makeAuthor(override?: partialAuthor): Author {
     return new Author({
-        bio: "test bio",
         name: new Name("test name"),
         password: new Password("Adk1!331d"),
         email: new Email("testemail@gmail.com"),
