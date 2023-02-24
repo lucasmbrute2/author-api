5acd142288aeb2e37df36f1d95c936598b49cbee
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-24 00:30:10 -0300

    feat: create function to create user in domain level -> Factory pattern

---
+++ src/application/modules/author/factory/makeAuthorRequest.ts
@@ -0,0 +1,15 @@
+import { RegisterAuthorRepositoryRequest } from "../use-cases/register-author-use-case";
+
+type partialAuthorRequest = Partial<RegisterAuthorRepositoryRequest>;
+
+export function makeUserInRequest(
+    override?: partialAuthorRequest
+): RegisterAuthorRepositoryRequest {
+    return {
+        email: "testemail@gmail.com",
+        password: "Amerau@1234",
+        name: "test name",
+        confirmPassword: '"Amerau@1234',
+        ...override,
+    };
+}
