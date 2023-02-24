5214dc648579049fc34ed5ef96268f80378fcd38
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-24 20:02:38 -0300

    feat: add auth author route

--- src/shared/infra/http/routes/author.routes.ts
+++ src/shared/infra/http/routes/author.routes.ts
@@ -1,9 +1,12 @@
 import { RegisterAuthorController } from "../../../../application/modules/author/use-cases/register-author-controller";
 import { Router } from "express";
+import { AuthAuthorController } from "../../../../application/modules/author/use-cases/auth-author-controller";
 const authorRouter = Router();
 
 const registerAuthorController = new RegisterAuthorController();
+const authAuthorController = new AuthAuthorController();
 
 authorRouter.post("/register", registerAuthorController.handle);
+authorRouter.post("/auth", authAuthorController.handle);
 
 export { authorRouter };
