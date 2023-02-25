f39c89c70efa715ded4e3bbd4251d1c89322fc28
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-25 20:39:28 -0300

    feat: add picture view model

---
+++ src/application/views/picture-view-model.ts
@@ -0,0 +1,24 @@
+import { Picture } from "@app/modules/picture/entities/picture";
+
+interface PictureViewModelProps {
+    name: string;
+    htmlUrl: string;
+    aliasKey: string;
+    id: string;
+    createdAt: Date;
+    galleryId: string;
+}
+
+export class PictureViewModel {
+    static toHTTP(file: Picture): PictureViewModelProps {
+        const { aliasKey, createdAt, galleryId, id, name, htmlUrl } = file;
+        return {
+            name,
+            aliasKey,
+            htmlUrl,
+            createdAt,
+            galleryId,
+            id,
+        };
+    }
+}
