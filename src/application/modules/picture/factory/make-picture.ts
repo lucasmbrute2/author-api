743c6630231307601693995cc71f5570b40c207a
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-25 20:52:04 -0300

    fix: receiving gallery id into make picture function

--- src/application/modules/picture/factory/make-picture.ts
+++ src/application/modules/picture/factory/make-picture.ts
@@ -2,9 +2,10 @@ import { Picture } from "../entities/picture";
 
 type OverridePicture = Partial<Express.Multer.File>;
 
-export function makePicture(file: OverridePicture): Picture {
+export function makePicture(file: OverridePicture, galleryId: string): Picture {
     const { originalname, path, filename } = file;
     return new Picture({
+        galleryId: galleryId || "",
         aliasKey: filename,
         htmlUrl: path,
         name: originalname,
