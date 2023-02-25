e93b8f2a0ad26ba3f3149e3ca6750221708f33ce
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-25 19:10:35 -0300

    feat: picture entity

---
+++ src/application/modules/picture/entities/picture.ts
@@ -0,0 +1,64 @@
+import { randomUUID } from "crypto";
+
+interface PictureProps {
+    id?: string;
+    htmlUrl: string;
+    name: string;
+    aliasKey: string;
+    createdAt: Date;
+    deletedAt?: Date;
+    galleryId?: string;
+}
+
+export class Picture {
+    constructor(private props: PictureProps) {
+        this.props = {
+            ...props,
+            id: props.id ?? randomUUID(),
+            galleryId: props.galleryId ?? randomUUID(),
+        };
+    }
+
+    get id(): string {
+        return this.props.id;
+    }
+
+    get htmlUrl(): string {
+        return this.props.htmlUrl;
+    }
+
+    set htmlUrl(htmlUrl: string) {
+        this.props.htmlUrl;
+    }
+
+    get name(): string {
+        return this.props.name;
+    }
+
+    set name(name: string) {
+        this.props.name;
+    }
+
+    get aliasKey(): string {
+        return this.props.aliasKey;
+    }
+
+    set aliasKey(aliasKey: string) {
+        this.props.aliasKey;
+    }
+
+    get createdAt(): Date {
+        return this.props.createdAt;
+    }
+
+    set deletedAt(deletedAt: Date) {
+        this.props.deletedAt;
+    }
+    get deletedAt(): Date {
+        return this.props.deletedAt;
+    }
+
+    get galleryId(): string {
+        return this.props.galleryId;
+    }
+}
