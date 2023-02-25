a102c8245f3b8dc2836deccaaaf6eeaaae697a33
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-25 12:35:13 -0300

    feat: add password use case and spec

---
+++ src/application/modules/author/use-cases/reset-password/reset-password-use-case.spec.ts
@@ -0,0 +1,61 @@
+import {
+    AppError,
+    BadRequestError,
+    NotFoundError,
+    Unauthorized,
+} from "@shared/errors/app-error";
+import { beforeEach, describe, expect, it, test } from "vitest";
+import { makeAuthor } from "../../factory/makeAuthor";
+import { InMemoryRepository } from "../../repository/in-memory-author-repository";
+import { ResetPasswordUseCase } from "./reset-password-use-case";
+
+let authorRepository: InMemoryRepository;
+let resetPasswordUseCase: ResetPasswordUseCase;
+
+describe("Reset password", () => {
+    beforeEach(() => {
+        authorRepository = new InMemoryRepository();
+        resetPasswordUseCase = new ResetPasswordUseCase(authorRepository);
+    });
+
+    it("should not be able to reset a password from no found author", () => {
+        expect(async () => {
+            await resetPasswordUseCase.execute({
+                id: "fake-user",
+                confirmNewPassword: "wrong-password",
+                newPassword: "wrong-new-password",
+                oldPassword: "worng-old-password",
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to change incorrect password", () => {
+        expect(async () => {
+            const author = makeAuthor();
+            await authorRepository.create(author);
+            const { id } = author;
+
+            await resetPasswordUseCase.execute({
+                id,
+                oldPassword: "Wrong-password123",
+                newPassword: "Ttest!123",
+                confirmNewPassword: "Ttest!123",
+            });
+        }).rejects.toBeInstanceOf(Unauthorized);
+    });
+
+    test("password and confirm password should be equal", () => {
+        expect(async () => {
+            const author = makeAuthor();
+            await authorRepository.create(author);
+            const { id, password } = author;
+
+            await resetPasswordUseCase.execute({
+                oldPassword: password.value,
+                id,
+                newPassword: "Equal!123",
+                confirmNewPassword: "Different!123",
+            });
+        }).rejects.toBeInstanceOf(BadRequestError);
+    });
+});
---
+++ src/application/modules/author/use-cases/reset-password/reset-password-use-case.ts
@@ -0,0 +1,53 @@
+import "reflect-metadata";
+import { AuthorRepository } from "../../../../repositories/author-repository";
+import {
+    BadRequestError,
+    NotFoundError,
+    Unauthorized,
+} from "../../../../../shared/errors/app-error";
+import { inject, injectable } from "tsyringe";
+import { compare, hash } from "bcryptjs";
+import { Password } from "../../entities/validation";
+
+interface ResetPasswordUseCaseProps {
+    id: string;
+    oldPassword: string;
+    newPassword: string;
+    confirmNewPassword: string;
+}
+
+@injectable()
+export class ResetPasswordUseCase {
+    constructor(
+        @inject("AuthorRepository")
+        private authorRepository: AuthorRepository
+    ) {}
+
+    async execute(req: ResetPasswordUseCaseProps): Promise<void> {
+        const { confirmNewPassword, id, newPassword, oldPassword } = req;
+
+        const author = await this.authorRepository.findByID(id);
+        if (!author) throw new NotFoundError("Author not found");
+
+        if (
+            new Password(newPassword).value !==
+            new Password(confirmNewPassword).value
+        )
+            throw new BadRequestError("Passwords not match");
+
+        const isPasswordCorrect = await compare(
+            oldPassword,
+            author.password.value
+        );
+
+        if (!isPasswordCorrect)
+            throw new Unauthorized("Email or password invalid");
+
+        const encryptedPassword = new Password(
+            await hash(newPassword, 8),
+            false
+        );
+
+        await this.authorRepository.savePassword(id, encryptedPassword);
+    }
+}
