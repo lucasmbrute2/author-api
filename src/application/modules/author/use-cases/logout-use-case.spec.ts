8db734df2c98231c11cff320d0756b9e33e4b278
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-25 02:55:03 -0300

    feat: add logout use case

---
+++ src/application/modules/author/use-cases/logout-use-case.spec.ts
@@ -0,0 +1,27 @@
+import { InMemoryRedisProvider } from "@shared/container/providers/RedisProvider/implementations/in-memory-redis-provider";
+import { beforeEach, describe, expect, it } from "vitest";
+import { makeAuthor } from "../factory/makeAuthor";
+import { makeUserInRequest } from "../factory/makeAuthorRequest";
+import { InMemoryRepository } from "../repository/in-memory-author-repository";
+import { LogoutUseCase } from "./logout-use-case";
+
+let logoutUseCase: LogoutUseCase;
+let redisRepository: InMemoryRedisProvider;
+let authorRepository: InMemoryRepository;
+
+describe("Logout", () => {
+    beforeEach(() => {
+        authorRepository = new InMemoryRepository();
+        redisRepository = new InMemoryRedisProvider();
+        logoutUseCase = new LogoutUseCase(redisRepository, authorRepository);
+    });
+
+    it("should be able to logout a user", () => {
+        expect(async () => {
+            const author = makeAuthor();
+            await authorRepository.create(author);
+
+            await logoutUseCase.execute(author.id);
+        }).not.toThrow;
+    });
+});
---
+++ src/application/modules/author/use-cases/logout-use-case.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { AuthorRepository } from "@app/repositories/author-repository";
+import { RedisRepository } from "@app/repositories/redis-repository";
+import { NotFoundError } from "@shared/errors/app-error";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export class LogoutUseCase {
+    constructor(
+        @inject("RedisRepository")
+        private redisClient: RedisRepository,
+        @inject("AuthorRepository")
+        private authorRepository: AuthorRepository
+    ) {}
+
+    async execute(id: string): Promise<void> {
+        const isAuthorValid = await this.authorRepository.findByID(id);
+        if (!isAuthorValid) throw new NotFoundError("Author not found.");
+
+        await this.redisClient.disconnect();
+        await this.redisClient.connect();
+
+        await this.redisClient.delete(id);
+    }
+}
