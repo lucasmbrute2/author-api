a376101811e59e377726bf46c02dd60c35065ebe
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-25 01:39:39 -0300

    feat: add edit profile controller and route

---
+++ src/application/modules/author/use-cases/edit-profile-controller.ts
@@ -0,0 +1,26 @@
+import { EditProfileProps } from "@app/repositories/author-repository";
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { EditProfileUseCase } from "./edit-profile-use-case";
+
+export class EditProfileController {
+    async handle(
+        req: Request<nonValuable, nonValuable, EditProfileProps>,
+        res: Response
+    ): Promise<Response> {
+        const editProfileUseCase = container.resolve(EditProfileUseCase);
+        const { id } = req.params;
+        const { bio, name, profile_picture } = req.body;
+
+        const author = await editProfileUseCase.execute(
+            {
+                bio,
+                name,
+                profile_picture,
+            },
+            id
+        );
+
+        return res.status(201).json(author);
+    }
+}
--- src/shared/infra/http/routes/author.routes.ts
+++ src/shared/infra/http/routes/author.routes.ts
@@ -1,12 +1,15 @@
 import { RegisterAuthorController } from "../../../../application/modules/author/use-cases/register-controller";
 import { Router } from "express";
 import { AuthAuthorController } from "../../../../application/modules/author/use-cases/auth-controller";
+import { EditProfileController } from "../../../../application/modules/author/use-cases/edit-profile-controller";
 const authorRouter = Router();
 
 const registerAuthorController = new RegisterAuthorController();
 const authAuthorController = new AuthAuthorController();
+const authEditProfileController = new EditProfileController();
 
 authorRouter.post("/register", registerAuthorController.handle);
 authorRouter.post("/auth", authAuthorController.handle);
+authorRouter.patch("/edit/:id", authEditProfileController.handle);
 
 export { authorRouter };
