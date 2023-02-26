306fef587a2006a60f3e90c6deb867f891a10471
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-26 04:01:02 -0300

    feat: Intancing make author with ID

--- src/application/modules/author/factory/makeAuthor.ts
+++ src/application/modules/author/factory/makeAuthor.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto";
 import { Author } from "../entities/author-entity";
 import { Email, Name, Password } from "../entities/validation";
 
@@ -5,6 +6,7 @@ type partialAuthor = Partial<Author>;
 
 export function makeAuthor(override?: partialAuthor): Author {
     return new Author({
+        id: randomUUID(),
         name: new Name("test name"),
         password: new Password("Adk1!331d"),
         email: new Email("testemail@gmail.com"),
