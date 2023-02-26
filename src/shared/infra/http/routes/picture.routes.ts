b673cfd913bc14a8b59a7b7f5c6811ac0bdfcf2f
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-26 18:32:55 -0300

    feat: add delete picture route

--- src/shared/infra/http/routes/picture.routes.ts
+++ src/shared/infra/http/routes/picture.routes.ts
@@ -4,18 +4,26 @@ import { Router } from "express";
 
 import multer from "multer";
 import { Authorization } from "../middlewares/ensure-auth";
+import { DeleteController } from "../../../../application/modules/picture/use-cases/delete/delete-controller";
 
 const pictureRoutes = Router();
 const upload = multer(multerConfigs);
 
 const authorization = new Authorization();
 const uploadController = new UploadController();
+const deleteController = new DeleteController();
 
 pictureRoutes.post(
     "/upload",
     authorization.ensureAuth,
     upload.single("pictures"),
     uploadController.handle
+);
+
+pictureRoutes.delete(
+    "/delete/:fileName",
+    authorization.ensureAuth,
+    deleteController.handle
 );
 
 export { pictureRoutes };
