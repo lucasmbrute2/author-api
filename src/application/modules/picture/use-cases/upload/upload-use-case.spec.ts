d9e9ae83ffdb342baf5d4fb48e7a011832916236
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-02-26 23:28:48 -0300

    refactor: remove spec & add build script

--- src/application/modules/picture/use-cases/upload/upload-use-case.spec.ts
+++ src/application/modules/picture/use-cases/upload/upload-use-case.spec.ts
@@ -1,8 +1,6 @@
-import { makeAuthor } from "@app/modules/author/factory/makeAuthor";
 import { InMemoryRepository } from "@app/modules/author/repository/in-memory-author-repository";
 import { LocalStorageProvider } from "@shared/container/providers/storage-provider/implementations/local-storage-provider";
 import { beforeEach, describe, expect, it } from "vitest";
-import { makePicture } from "../../factory/make-picture";
 import { InMemoryPictureRepository } from "../../repository/in-memory-picture-repository";
 import { UploadPictureUseCase } from "./upload-use-case";
 
@@ -21,13 +19,5 @@ describe("Upload picture", () => {
             authorRepository,
             pictureRepository
         );
-    });
-
-    it("should not be able to post a picture without an author ID", () => {
-        expect(async () => {
-            const picture = makePicture();
-            const author = makeAuthor();
-            await uploadPictureUseCase.execute(picture, author.id);
-        });
     });
 });
