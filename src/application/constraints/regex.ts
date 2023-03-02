4c873b13d151498d3d1e6c358a5c42275a0ed643
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-02 02:06:18 -0300

    fix: regex name validation

--- src/application/constraints/regex.ts
+++ src/application/constraints/regex.ts
@@ -2,6 +2,6 @@ export const validation = {
     passwordValidation: new RegExp(
         /(?=^.{8,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g
     ),
-    nameValidation: new RegExp(/^[a-zA-Z ]{3,20}$/),
+    nameValidation: new RegExp(/^[a-zA-Z\u00E0-\u00FC ]{3,20}$/),
     emailValidation: new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
 };
