c0f80c6b349a86b64295e556e33fce92d10a12ae
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-03-08 22:06:19 -0300

    fix: fill with necessary dependencies and add refresh token

--- src/application/modules/author/use-cases/auth/auth-use-case.spec.ts
+++ src/application/modules/author/use-cases/auth/auth-use-case.spec.ts
@@ -1,3 +1,6 @@
+import { makeRefreshToken } from "@app/modules/refresh-token/factory/make-refresh-token";
+import { InMemoryRefreshTokenRepository } from "@app/modules/refresh-token/repository/in-memory-refresh-token";
+import { InMemoryDateProvider } from "@shared/container/providers/date-provider/implementations/in-memory-date-provider";
 import { RedisProvider } from "@shared/container/providers/redis-provider/implementations/redis-provider";
 import { BadRequestError, NotFoundError } from "@shared/errors/app-error";
 import "reflect-metadata";
@@ -6,24 +9,37 @@ import { makeAuthor } from "../../factory/makeAuthor";
 import { makeUserInRequest } from "../../factory/makeAuthorRequest";
 import { InMemoryRepository } from "../../repository/in-memory-author-repository";
 import { AuthAuthorUseCase } from "./auth-use-case";
+import { randomUUID } from "node:crypto";
 
 let authAuthorUseCase: AuthAuthorUseCase;
 let authorRepository: InMemoryRepository;
 let redisProvider: RedisProvider;
+let dateProvider: InMemoryDateProvider;
+let refreshTokenRepository: InMemoryRefreshTokenRepository;
 
 describe("Auth Author", () => {
     beforeEach(() => {
         authorRepository = new InMemoryRepository();
+        dateProvider = new InMemoryDateProvider();
+        refreshTokenRepository = new InMemoryRefreshTokenRepository();
         authAuthorUseCase = new AuthAuthorUseCase(
             authorRepository,
-            redisProvider
+            redisProvider,
+            dateProvider,
+            refreshTokenRepository
         );
     });
 
-    it("should throw a error if user is not found", () => {
+    it("should throw NotFoundError if user is not found", () => {
         expect(async () => {
-            const author = makeUserInRequest();
-            await authAuthorUseCase.execute(author);
+            const { email, password } = makeUserInRequest();
+            const refreshToken = makeRefreshToken(randomUUID());
+
+            await authAuthorUseCase.execute({
+                email,
+                password,
+                refreshToken: refreshToken.token,
+            });
         }).rejects.toBeInstanceOf(NotFoundError);
     });
 
@@ -31,6 +47,7 @@ describe("Auth Author", () => {
         expect(async () => {
             const author = makeAuthor();
             authorRepository.create(author);
+            const { token: refreshToken } = makeRefreshToken(author.id);
             const {
                 email: { value: emailValue },
                 password: { value: passwordValue },
@@ -39,19 +56,22 @@ describe("Auth Author", () => {
             await authAuthorUseCase.execute({
                 email: emailValue,
                 password: passwordValue,
+                refreshToken,
             });
-        }).not.toThrow;
+        }).rejects.toThrow();
     });
 
     it("should not be able to auth a user with wrong password", () => {
         expect(async () => {
             const author = makeAuthor();
             await authorRepository.create(author);
+            const { token: refreshToken } = makeRefreshToken(author.id);
 
             return await authAuthorUseCase.execute({
                 email: author.email.value,
                 password: "wrong-password",
+                refreshToken,
             });
-        }).rejects.toThrow("Email or password incorrect");
+        }).rejects.toBeInstanceOf(BadRequestError);
     });
 });
